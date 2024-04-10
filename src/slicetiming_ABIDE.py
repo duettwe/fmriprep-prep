0343132edec5123d4615c3a57fec9b3501527fd8
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-04-10 11:45:13 -0500

    ABIDEtiming

--- src/slicetiming_ABIDE.py
+++ src/slicetiming_ABIDE.py
@@ -13,8 +13,40 @@ site = args.subject_label.split('_')[0]
 
 if site=='Caltech':
     print(site)
+elif site=='CMU':
+    print(site)
+elif site=='KKI':
+    print(site)
+elif site=='Leuven':
+    print(site)
+elif site=='MaxMun':
+    print(site)
 elif site=='NYU':
+    print(site)
+elif site=='OHSU':
+    print(site)
+elif site=='Olin':
+    print(site)
+elif site=='Pitt':
+    print(site)
+elif site=='SBL':
+    print(site)
+elif site=='SDSU':
+    print(site)
+elif site=='Stanford':
+    print(site)
+elif site=='Trinity':
+    print(site)
+elif site=='UCLA':
+    print(site)
+elif site=='UM':
+    print(site)
+elif site=='USM':
+    print(site)
+elif site=='Yale':
     print(site)
 else:
     raise Exception(f'Unknown ABIDE site {site} for subject {args.subject_label}')
+
+
 
