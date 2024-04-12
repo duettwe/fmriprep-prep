41a94546c6af6e298cbbbed27ac754a1da017b0f
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-04-12 16:36:57 -0500

    nojson

---
+++ src/create_json.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import math
+import nibabel
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--fmri_niigz')
+parser.add_argument('--slicetiming')
+args = parser.parse_args()
+
+
+# Initialize empty json
+json_data = {}
+
+# Get some info from .nii.gz. Assume slice dim is dim[3] (k)
+nii = nibabel.load(args.fmri_niigz)
+nslices = nii.header['dim'][3]
+tr = nii.header['pixdim'][4]
+
+# fmriprep minimum needed fields
+json_data['RepetitionTime'] = f'{tr}'
+
+## Slice timing
+# Assumes slices are evenly spaced in the TR (i.e. not a sparse
+# acquisition, and any extra inter-TR delay is minimal)
+
+if args.slicetiming:
+
+    # Get a base list of ascending slice times that we will re-order
+    basetimes = [x / nslices * tr for x in range(0,nslices)]
+
+    # We can only handle certain specific cases
+    if args.slicetiming in ['Philips_ASCEND_k', 'Siemens_ascending_k']:
+        json_data['SliceEncodingDirection'] = 'k'
+        json_data['SliceTiming'] = basetimes
+
+    elif args.slicetiming in ['Philips_DESCEND_k', 'Siemens_descending_k']:
+        json_data['SliceEncodingDirection'] = 'k'
+        json_data['SliceTiming'] = list(reversed(basetimes))
+
+    elif args.slicetiming in ['Siemens_interleaved_k']:
+        json_data['SliceEncodingDirection'] = 'k'
+        json_data['SliceTiming'] = [0 for x in basetimes]
+        if nslices % 2 == 0:  # Even number of slices
+            json_data['SliceTiming'][1::2] = basetimes[0:math.ceil(nslices/2)]
+            json_data['SliceTiming'][0::2] = basetimes[math.ceil(nslices/2):]
+        else: # Odd number of slices
+            json_data['SliceTiming'][0::2] = basetimes[0:math.ceil(nslices/2)]
+            json_data['SliceTiming'][1::2] = basetimes[math.ceil(nslices/2):]
+
+    elif args.slicetiming in ['GE_interleaved_k']:
+        json_data['SliceEncodingDirection'] = 'k'
+        json_data['SliceTiming'] = [0 for x in basetimes]
+        json_data['SliceTiming'][0::2] = basetimes[0:math.ceil(nslices/2)]
+        json_data['SliceTiming'][1::2] = basetimes[math.ceil(nslices/2):]
+        
+    elif args.slicetiming in ['none']:
+        pass
+
+    else:
+        raise Exception(f'Cannot handle slice timing of {args.slicetiming}')
+
+## Print it out
+print(json.dumps(json_data, indent = 4))
---
+++ src/fmriprep-prep-nojson.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+#
+# Entrypoint for BIDS formatting
+
+# Initialize default inputs
+export t1_niigz=/INPUTS/t1.nii.gz
+export fmri_niigzs=/INPUTS/fmri.nii.gz
+export rpefwd_niigz=""
+export rperev_niigz=""
+export slicetiming=""
+export fmritask=fmritask
+export fmrirun=1
+export bids_dir=/INPUTS/BIDS
+export sub=01
+export ses=01
+
+# Parse input options. Bit of extra gymnastics to allow multiple files
+# for fMRIs. We will assume no .nii.gz have the matching .json sidecar,
+# so will create those from scratch in the same location with the same 
+# base filename.
+t1_list=()
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in      
+        --t1_niigz)       export t1_niigz="$2";       shift; shift ;;
+        --rpefwd_niigz)   export rpefwd_niigz="$2";   shift; shift ;;
+        --rperev_niigz)   export rperev_niigz="$2";   shift; shift ;;
+        --bids_dir)       export bids_dir="$2";       shift; shift ;;
+        --sub)            export sub="$2";            shift; shift ;;
+        --ses)            export ses="$2";            shift; shift ;;
+        --slicetiming)    export slicetiming="$2";    shift; shift ;;
+        --fmritask)       export fmritask="$2";       shift; shift ;;
+        --fmrirun)        export fmrirun="$2";        shift; shift ;;
+        --fmri_niigzs)
+            next="$2"
+            while ! [[ "$next" =~ -.* ]] && [[ $# > 1 ]]; do
+                fmri_list+=("$next")
+                shift
+                next="$2"
+            done
+            shift ;;
+        *) echo "Input ${1} not recognized"; shift ;;
+    esac
+done
+
+# Count fmris
+export num_fmris=${#fmri_list[@]}
+
+# Show a bit of info
+echo "Subject ${sub}, session ${ses}"
+echo "T1: ${t1_niigz}"
+echo "fMRIs (${num_fmris}): ${fmri_list[@]}"
+
+
+# If slicetiming is 'ABIDE', use subject label to determine actual ordering
+# and update the value of slicetiming
+if [ "${slicetiming}" = "ABIDE" ]; then
+    export slicetiming=$(slicetiming_ABIDE.py --subject_label "${sub}")
+fi
+
+
+# Rename and relocate files according to bids scheme
+
+# T1 scan
+mkdir -p "${bids_dir}/sub-${sub}/ses-${ses}/anat"
+t1_tag="sub-${sub}/ses-${ses}/anat/sub-${sub}_ses-${ses}_T1w"
+cp "${t1_niigz}" "${bids_dir}/${t1_tag}.nii.gz"
+
+# fMRIs
+mkdir -p "${bids_dir}/sub-${sub}/ses-${ses}/func"
+for fmri_niigz in ${fmri_list[@]}; do
+    
+    fmri_json="${fmri_niigz%.nii.gz}.json"
+    
+    intended_tag="ses-${ses}/func/sub-${sub}_ses-${ses}_task-${fmritask}_run-${fmrirun}_bold"
+    fmri_tag="sub-${sub}/${intended_tag}"
+
+    cp "${fmri_niigz}" "${bids_dir}/${fmri_tag}.nii.gz"
+    create_json.py --fmri_niigz ${fmri_niigz} --slicetiming "${slicetiming}" \
+        > "${bids_dir}/${fmri_tag}.json"
+
+done
+
+# Finally, the required dataset description file
+echo '{"Name": "Ready for fmriprep", "BIDSVersion": "1.9.0"}' > "${bids_dir}/dataset_description.json"
+
